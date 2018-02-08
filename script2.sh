96514042c5684e21be1ac092556d46d958dca812
Aneeshtgeorge <35955684+Aneeshtgeorge@users.noreply.github.com>
2018-02-08 11:28:58 +0530

    Add files via upload

---
+++ script2.sh
@@ -0,0 +1,16 @@
+x=$(logname)
+echo "Currently logged user and his logname:$x"
+
+echo $SHELL
+
+echo $HOME
+
+x=$(arch)
+echo "Your operating system type :$x"
+
+echo $PATH
+
+echo $PWD
+
+
+echo $USER
